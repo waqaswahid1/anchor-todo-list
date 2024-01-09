623d8fb823a3fc52674434e7cd942375ebe54ae5
waqasideofuzion <waqaswahid1>
2024-01-09 23:33:35 +0500

    config updated

--- programs/todo-list-app/src/lib.rs
+++ programs/todo-list-app/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("5wjSz6N7WWs5R6Ym4TArxrvGy76P6o2SHyoiHSJZkyWG");
 
 #[program]
 pub mod todo_list_app {
@@ -10,47 +10,44 @@ pub mod todo_list_app {
         let task = &mut ctx.accounts.task;
         let author = &ctx.accounts.author; // The `author` account
         let clock = Clock::get().unwrap(); // Getting the current timestamp
-        
         if text.chars().count() > 400 {
             return Err(ErrorCode::TextTooLong.into());
         }
-        
         task.author = *author.key;
         task.is_done = false;
         task.created_at = clock.unix_timestamp;
         task.updated_at = clock.unix_timestamp;
         task.text = text;
         Ok(())
+    }
 
+    #[error_code]
+    pub enum ErrorCode {
+        #[msg("The text is too long")]
+        TextTooLong,
     }
 
     pub fn updating_task(ctx: Context<UpdatingTask>, is_done: bool) -> Result<()> {
         let task = &mut ctx.accounts.task;
         let author = &ctx.accounts.author; // The `author` account
         let clock = Clock::get().unwrap(); // Getting the current timestamp
-        
+
         task.author = *author.key;
         task.is_done = is_done;
         task.updated_at = clock.unix_timestamp;
         Ok(())
-       
     }
 
     pub fn deleting_task(ctx: Context<DeletingTask>) -> Result<()> {
         let task = &mut ctx.accounts.task;
         let author = &ctx.accounts.author; // The `author` account
         let clock = Clock::get().unwrap(); // Getting the current timestamp
-        
+
         task.author = *author.key;
         task.is_done = true;
         task.updated_at = clock.unix_timestamp;
         Ok(())
-       
     }
-
-
-
-
 }
 
 #[derive(Accounts)]
@@ -61,7 +58,6 @@ pub struct AddingTask<'info> {
     pub author: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
-
 
 #[derive(Accounts)]
 pub struct UpdatingTask<'info> {
@@ -77,7 +73,6 @@ pub struct DeletingTask<'info> {
     pub author: Signer<'info>,
 }
 
-
 #[account]
 pub struct Task {
     pub author: Pubkey,  // The account that owns the task
@@ -90,7 +85,7 @@ pub struct Task {
 const DISCRIMINATOR: usize = 8;
 const PUBLIC_KEY_LENGTH: usize = 32;
 const BOOL_LENGTH: usize = 1;
-const TEXT_LENGTH: usize = 4 + 400 * 4; // 400 chars
+const TEXT_LENGTH: usize = 4 + 400 * 4; // 400 chars // 4 bytes max length for UTF-8 encoding in strings
 const TIMESTAMP_LENGTH: usize = 8;
 
 impl Task {
@@ -101,7 +96,6 @@ impl Task {
         TIMESTAMP_LENGTH + // created_at
         TIMESTAMP_LENGTH; // updated_at
 }
-
 
 #[error_code]
 pub enum ErrorCode {
